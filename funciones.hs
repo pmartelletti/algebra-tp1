2500a381cbc0bac2ef5bc77cb11249b01985b33e
Pablo Martelletti <pmartelletti@gmail.com>
2013-10-06 01:54:29 -0300

    Agrego ejercicio 5 y algunas funciones extras que pueden ser de utilidad.

--- funciones.hs
+++ funciones.hs
@@ -1,7 +1,29 @@
-s1 :: Integer -> Integer
+import Data.List
+import Data.Maybe
+
+-- ejercicio 1
+s1 :: Int -> Int
 s1 1 = (2*1 - 1)^2
 s1 n = s1(n-1) + (2*n-1)^2
 
-s2 :: Integer -> Integer
+-- ejercicio 2
+s2 :: Int -> Int
 s2 1 = ((-1)^1) * 2^1
 s2 n = s2(n-1) + ((-1)^n) * 2^n
+
+-- funciones auxiliares
+esPrimo :: Integer -> Bool
+esPrimo x = null (filter (\y ->  x `mod`y == 0) (takeWhile (\y ->  y*y <= x) [2..]))
+
+-- tecnica similar a la criba de erastotenes, pero para un solo numero
+menorPrimoDivisor :: Integer-> Integer
+menorPrimoDivisor x = fromJust (find(\y -> (x `mod` y) == 0 ) [2..])
+
+
+-- ejercicio 5
+factoresPrimos :: Integer -> [Integer]
+factoresPrimos n | n < 2 = []
+-- puedo usar division entera porque se que p divide a n porque p es primo
+factoresPrimos n = p : factoresPrimos(n `div` p)
+		where p = menorPrimoDivisor n
+
