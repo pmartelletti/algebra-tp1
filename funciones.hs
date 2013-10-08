80c8372d231dc47806d6927e8fc3c2e3035b5e89
Pablo Martelletti <pmartelletti@gmail.com>
2013-10-08 00:19:01 -0300

    Cambios en el ejercicio 5 para que solo haga uso de funciones vistas en clase.

--- funciones.hs
+++ funciones.hs
@@ -15,22 +15,27 @@ s2 n = s2(n-1) + ((-1)^n) * 2^n
 elementosRepetidos :: [Int] -> [Int] -> Int
 elementosRepetidos x [] = 0
 elementosRepetidos [] y = 0
-elementosRepetidos (x:xs) y | x `elem` y = 1 + elementosRepetidos xs y
+--elementosRepetidos (x:xs) y | x `elem` y = 1 + elementosRepetidos xs y
+--elementosRepetidos (x:xs) y = elementosRepetidos xs y
+elementosRepetidos (x:xs) (y:ys) | x == y = 1 + elementosRepetidos xs ys
 elementosRepetidos (x:xs) y = elementosRepetidos xs y
 
--- funciones auxiliares
-esPrimo :: Integer -> Bool
-esPrimo x = null (filter (\y ->  x `mod`y == 0) (takeWhile (\y ->  y*y <= x) [2..]))
+-- funcione auxiliares para los otros ejercicios
+-- encuentra el primer divisor en una lista (no tiene por que ser el menor)
+primerDivisorEn :: [Int] -> Int -> Int
+primerDivisorEn [] n = 0
+primerDivisorEn (x:xs) n | ((n `mod` x) == 0) = x
+primerDivisorEn (x:xs) n = primerDivisorEn xs n
 
 -- tecnica similar a la criba de erastotenes, pero para un solo numero
-menorPrimoDivisor :: Integer-> Integer
-menorPrimoDivisor x = fromJust (find(\y -> (x `mod` y) == 0 ) [2..])
+-- se que siempre es el menor porque la lista [2..] esta ordenada de menor a mayor
+menorPrimoDivisor :: Int-> Int
+menorPrimoDivisor x = primerDivisorEn [2..] x
 
 
 -- ejercicio 5
-factoresPrimos :: Integer -> [Integer]
+factoresPrimos :: Int -> [Int]
 factoresPrimos n | n < 2 = []
 -- puedo usar division entera porque se que p divide a n porque p es primo
 factoresPrimos n = p : factoresPrimos(n `div` p)
 		where p = menorPrimoDivisor n
-
