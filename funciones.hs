58b70e8aa6028614a475a89f4c420ed54a3d313e
Pablo Martelletti <pmartelletti@gmail.com>
2013-10-09 16:11:42 -0300

    Arreglo los problemas que tenia el ejercicio 3, de acuerdo a la respuesta que me dio Fede Lebrón en la lista de docentes.

--- funciones.hs
+++ funciones.hs
@@ -1,6 +1,3 @@
-import Data.List
-import Data.Maybe
-
 -- ejercicio 1
 s1 :: Int -> Int
 s1 1 = (2*1 - 1)^2
@@ -11,24 +8,23 @@ s2 :: Int -> Int
 s2 1 = ((-1)^1) * 2^1
 s2 n = s2(n-1) + ((-1)^n) * 2^n
 
+-- funcion que dada una lista, dice si el el elemento y está al menos una vez en la misma
+pertenece :: [Int] -> Int -> Bool
+pertenece [] y = False
+pertenece (x:xs) y | x == y = True
+                   | x /= y = pertenece xs y
+
 -- ejercicio 3
 elementosRepetidos :: [Int] -> [Int] -> Int
 elementosRepetidos x [] = 0
 elementosRepetidos [] y = 0
---elementosRepetidos (x:xs) y | x `elem` y = 1 + elementosRepetidos xs y
---elementosRepetidos (x:xs) y = elementosRepetidos xs y
-elementosRepetidos (x:xs) (y:ys) | x == y = 1 + elementosRepetidos xs ys
+elementosRepetidos (x:xs) y | pertenece y x = 1 + elementosRepetidos xs y
 elementosRepetidos (x:xs) y = elementosRepetidos xs y
 
-
+-- devuelve la longitud de una lista
 long :: [a] -> Int
 long [] = 0
 long (x:xs) = 1 + (long xs)
-
--- ejercicio 4
-triangular :: [Int] -> Bool
-triangular (x:y:ys) | x <= y = triangular(y:ys)
-		    | x > y = decreciente(y:ys)
 
 -- funcion que decide si una lista es decreciente
 decreciente :: [Int] -> Bool
@@ -37,7 +33,12 @@ decreciente x | long(x) == 1 = True
 decreciente (x:y:ys) | x >= y = decreciente (y:ys)
 decreciente x = False
 
--- funcione auxiliares para los otros ejercicios
+-- ejercicio 4
+triangular :: [Int] -> Bool
+triangular (x:y:ys) | x <= y = triangular(y:ys)
+		    | x > y = decreciente(y:ys)))
+
+
 -- encuentra el primer divisor en una lista (no tiene por que ser el menor)
 primerDivisorEn :: [Int] -> Int -> Int
 primerDivisorEn [] n = 0
